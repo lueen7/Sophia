7cbfc81571a279e2c67ca7f6d09eee00f8dd5486
nwankwosobe emmanuel <emmaosobe7@gmail.com>
2025-09-05 11:17:21 +0100

    New Change

--- src/Components/ChatInput.jsx
+++ src/Components/ChatInput.jsx
@@ -16,6 +16,8 @@ const ChatInput = ({
           : "bg-white border-t border-gray-200"
       } p-4`}
     >
+
+      {/* New changes  */}
       <div className="max-w-5xl mx-auto">
         <div className="flex items-center space-x-3">
           <input
